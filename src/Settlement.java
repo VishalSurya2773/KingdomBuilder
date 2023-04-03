9c495b8cb8dcd6a12ed176cfb3391585d89903f6
Samanvita Kasibhatla <lil.kasibhatla@gmail.com>
2023-04-03 07:20:43 -0500

    settlement

--- src/Settlement.java
+++ src/Settlement.java
@@ -1,3 +1,16 @@
 public class Settlement {
+    private int x, y;
+
+    public Settlement(int player) {
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
 }
