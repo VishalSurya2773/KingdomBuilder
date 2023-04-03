a00fed6b442d4e73fe24d27f1f79b62e4ae663c2
Samanvita Kasibhatla <lil.kasibhatla@gmail.com>
2023-04-03 07:39:59 -0500

    obj cards

--- src/ObjectiveCard.java
+++ src/ObjectiveCard.java
@@ -11,10 +11,44 @@ public class ObjectiveCard {
     }
 
     public int citizen(String settlementColor) {
+        // find largest cluster of adjacent settlements
+        // add 2 points for
         return 0;
     }
 
     public int discoverer(String settlementColor) {
+        return 0;
+    }
+
+    public int farmer(String settlementColor) {
+        return 0;
+    }
+
+    public int fishermen(String settlementColor) {
+        return 0;
+    }
+
+    public int hermit(String settlementColor) {
+        return 0;
+    }
+
+    public int knight(String settlementColor) {
+        return 0;
+    }
+
+    public int lord(String settlementColor) {
+        return 0;
+    }
+
+    public int merchant(String settlementColor) {
+        return 0;
+    }
+
+    public int miner(String settlementColor) {
+        return 0;
+    }
+
+    public int worker(String settlementColor) {
         return 0;
     }
 }
