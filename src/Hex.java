6d630cbd37081299937e615b6db93b2b4f599898
GerardZhou <dorey1200@gmail.com>
2023-04-03 07:57:34 -0500

    bsrbdsbfd

--- src/Hex.java
+++ src/Hex.java
@@ -2,10 +2,15 @@ public class Hex {
     private Hex left, right, topLeft, topRight, bottomLeft, bottomRight;
     private int x, y;
     private String terrain;
-
-    public Hex() {
+    private Settlement playerSettlement; // stores the settlement (if it's been placed on this specific hex)
+    public Hex(int xComp, int yComp, String terrainComp) {
+        x = xComp;
+        y = yComp;
+        terrain = terrainComp;
     }
-
+    public void setSettlement(Settlement s){
+        playerSettlement = s;
+    }
     public int getCenterX() {
         return x;
     }
