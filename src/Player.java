d3498f4cd64740d1e171a6e090a35a7da05023ef
Samanvita Kasibhatla <lil.kasibhatla@gmail.com>
2023-04-02 20:14:08 -0500

    initializing classes

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,52 @@
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<SpecialHex> hand;
+    private ArrayList<Settlement> placed;
+    private int score;
+    private boolean isFirst;
+
+    public Player(boolean first) {
+        isFirst = first;
+    }
+
+    public void setFirst() {
+        isFirst = true;
+    }
+
+    public void addSettlementToBoard() {
+    }
+
+    public void findAdjacentSettlements() {
+    }
+
+    public Settlement getSettlement() {
+        // random return filler
+        return placed.get(0);
+    }
+
+    public int getScore() {
+        return 0;
+    }
+
+    public void calculateScore() {
+    }
+
+    public void pullCard() {
+    }
+
+    public void useSpecialHexTile() {
+    }
+
+    public void addSpecialHexTile(SpecialHex x) {
+        hand.add(x);
+    }
+
+    public void removeSpecialHexTile(SpecialHex x) {
+        int pl = hand.indexOf(x);
+        if (pl >= 0) {
+            hand.remove(pl);
+        }
+    }
 
 }
