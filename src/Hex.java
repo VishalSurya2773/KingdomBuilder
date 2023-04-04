2db7f19c7f565c16d6b80619bbeaf7744b1cb7db
Samanvita Kasibhatla <lil.kasibhatla@gmail.com>
2023-04-03 20:43:18 -0500

    hex classes

--- src/Hex.java
+++ src/Hex.java
@@ -3,14 +3,17 @@ public class Hex {
     private int x, y;
     private String terrain;
     private Settlement playerSettlement; // stores the settlement (if it's been placed on this specific hex)
+
     public Hex(int xComp, int yComp, String terrainComp) {
         x = xComp;
         y = yComp;
         terrain = terrainComp;
     }
-    public void setSettlement(Settlement s){
+
+    public void setSettlement(Settlement s) {
         playerSettlement = s;
     }
+
     public int getCenterX() {
         return x;
     }
