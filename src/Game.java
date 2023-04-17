13aa225807153976fb9da3dc50fbb013504112fb
GerardZhou <dorey1200@gmail.com>
2023-04-17 07:27:58 -0500

    njv nvdsn njk

--- src/Game.java
+++ src/Game.java
@@ -87,7 +87,7 @@ public class Game {
             playerIndex %= 4;
             Player p = players.get(playerIndex);
 
-            if(players.get(playerIndex).numSettlements() == 0 && stop == -1) stop = playerIndex;
+            if(players.get(playerIndex).numSettlements() == 0 && stop == -1) {stop = playerIndex;}
         } 
     }
 
