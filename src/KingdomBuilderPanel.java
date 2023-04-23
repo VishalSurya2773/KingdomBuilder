ac0ca6ff4b591699e26dd074029b668b43f611ed
GerardZhou <dorey1200@gmail.com>
2023-04-23 15:52:16 -0500

    mkelr

--- src/KingdomBuilderPanel.java
+++ src/KingdomBuilderPanel.java
@@ -165,7 +165,7 @@ public class KingdomBuilderPanel extends JPanel implements MouseListener, Action
             g.setColor(burgundy);
 
             drawBoard(g);
-            drawHexOutline(g);
+            // drawHexOutline(g);
             // image.png(g);
 
             g.drawRect(0, 128, 340, 340);
