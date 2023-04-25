15174bf8b61c84880f867ece1f5974640e0520ef
Samanvita Kasibhatla <lil.kasibhatla@gmail.com>
2023-04-24 20:22:44 -0500

    checking board

--- src/Board.java
+++ src/Board.java
@@ -66,7 +66,7 @@ public class Board {
         for (int r = 0; r < Graph.length; r++) {
             for (int c = 0; c < Graph[r].length; c++) {
                 if ((r % 2 == 0 && c % 2 == 1) || (r % 2 == 1 && c % 2 == 0)) {
-                    Graph[r][c] = new Hex(""); // basically just a null pointer
+                    Graph[r][c] = new Hex("blank"); // basically just a null pointer
                     Graph[r][c].setAdjacent();
                     Graph[r][c].setX(coordX);
                     Graph[r][c].setY(coordY);
@@ -88,7 +88,6 @@ public class Board {
                             Graph[r][c].setX(coordX);
                             Graph[r][c].setY(coordY);
                             coordX += changeX;
-
                         }
                     }
                 }
@@ -156,7 +155,7 @@ public class Board {
                 if (Graph[i][j] == null)
                     System.out.print("N");
                 else
-                    System.out.print(" " + Graph[i][j]);
+                    System.out.print(" " + Graph[i][j].getTerrain().substring(0, 1));
             }
             System.out.println();
         }
