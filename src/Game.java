942aba3846c708b1a6b24f13c4a9954179e280c6
Samanvita Kasibhatla <lil.kasibhatla@gmail.com>
2023-05-09 08:01:32 -0500

    derandomizing players

--- src/Game.java
+++ src/Game.java
@@ -37,7 +37,7 @@ public class Game {
             players.add(new Player(false, clrs.get(temp - 1), i));
             clrs.remove(clrs.get(temp - 1));
         }
-        Collections.shuffle(players);
+        // Collections.shuffle(players);
         for (Player p : players) {
             System.out.print(p.getOrder() + ", " + p.getColor() + "; ");
         }
